cab345a11bd2dc3ecfb1143ed0de51acb4028c60
Anatoly Pavlishen <anatolpaa@gmail.com>
2014-01-21 23:55:45 +0200

    change gulpfile.js: add task for livereload server and refresh for other tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,41 @@ var gulp = require('gulp'),
     minifiCss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    refresh = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
 
-gulp.task('template:jade', function(){
+gulp.task('template:jade', function() {
     return gulp.src('./layouts/*.jade')
         .pipe(jade())
-        .pipe(gulp.dest('./assets/html'));
+        .pipe(gulp.dest('./assets/html'))
+        .pipe(refresh(server));
 });
 
-gulp.task('css:sass', function(){
+gulp.task('css:sass', function() {
     gulp.src('./components/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(refresh(server));
 });
 
 gulp.task('js:scripts', function() {
     // Minify and copy all JavaScript
     return gulp.src('./components/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./assets/js'));
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(refresh(server));
 });
 
-gulp.task('default', function(){
-    gulp.run('template:jade', 'css:sass', 'js:scripts');
+gulp.task('lr-server', function() {
+    server.listen(35729, function(err) {
+        if(err) return console.log(err);
+    });
+});
+
+gulp.task('default', function() {
+    gulp.run('lr-server', 'template:jade', 'css:sass', 'js:scripts');
 
     // add watchers to jade, scss, js files
     gulp.watch('./layouts/*.jade', function(){
