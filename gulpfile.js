1b9bac313c057c3b642e66f452b637ea7c29662c
Anatoly Pavlishen <anatolpaa@gmail.com>
2014-02-06 03:01:44 +0200

    change project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
-var gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    jade = require('gulp-jade'),
-    minifyCss = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    refresh = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
-
-
+var gulp        = require('gulp'),
+    clean       = require('gulp-clean'),
+    jade        = require('gulp-jade'),
+    minifyCss   = require('gulp-minify-css'),
+    concat      = require('gulp-concat'),
+    rename      = require('gulp-rename'),
+    sass        = require('gulp-sass'),
+    uglify      = require('gulp-uglify'),
+    imagemin    = require('gulp-imagemin'),
+    cache       = require('gulp-cache'),
+    refresh     = require('gulp-livereload'),
+    lr          = require('tiny-lr'),
+    server      = lr();
 
 var paths = {
     jade   : './layouts/*.jade',
-    styles   : './components/styles.scss',
+    styles : './components/styles.scss',
     scripts: './components/**/*.js',
     images : ['./components/**/*.png', './components/**/*.jpg'],
     fonts  : ['./components/b-page/fonts/*']
@@ -31,27 +30,24 @@ gulp.task('jade', function () {
 gulp.task('styles', function () {
     return gulp.src(paths.styles)
         .pipe(sass({ includePaths: ['./components/'] }))
-//        .pipe(concat('styles.css'))
         .pipe(minifyCss())
         .pipe(gulp.dest('./assets/css'))
-        .pipe(refresh(server))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(refresh(server));
 });
 
 gulp.task('scripts', function () {
     // Minify and copy all JavaScript
     return gulp.src(paths.scripts)
-        .pipe(concat('main.js'))
+//        .pipe(concat('main.js'))
         .pipe(gulp.dest('./assets/js'))
         .pipe(refresh(server));
 });
 
 gulp.task('images', function () {
     return gulp.src(paths.images)
-        .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+        .pipe(imagemin())
         .pipe(gulp.dest('./assets/images'))
-        .pipe(refresh(server))
-        .pipe(notify({ message: 'Images task complete' }));
+        .pipe(refresh(server));
 });
 
 gulp.task('fonts', function () {
@@ -60,26 +56,30 @@ gulp.task('fonts', function () {
         .pipe(refresh(server));
 });
 
+// Livereload
 gulp.task('lr-server', function () {
     server.listen(35729, function (err) {
         if (err) return console.log(err);
     });
 });
 
-gulp.task('clean', function() {
-    return gulp.src(['./assets/css', './assets/js', './assets/'], {read: false})
+// Clean
+gulp.task('clean', function () {
+    return gulp.src(['./assets/'], {read: false})
         .pipe(clean());
 });
 
-// Rerun the task when a file changes
+// Watch
 gulp.task('watch', function () {
     gulp.watch(paths.jade, ['jade']);
-    gulp.watch(paths.styles, ['styles']);
+    gulp.watch('./components/**/*.scss', ['styles']);
     gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.images, ['images']);
     gulp.watch(paths.fonts, ['fonts']);
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['lr-server', 'jade', 'styles', 'scripts', 'images', 'fonts', 'watch']);
 
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('jade', 'styles', 'scripts', 'images', 'fonts','lr-server', 'watch');
+});
